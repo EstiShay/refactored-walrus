da8173bbef579048aeb54760b9ff235708aabf50
Bart Webber and Esti Shay <esti@>
2017-07-10 11:52:44 -0700

    refactor code to use arrays and forEach.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-   $("h1").click(function() {
-     alert("This is a header.");
-   });
-   $("p").click(function() {
-     alert("This is a paragraph.");
-   });
-   $("img").click(function() {
-     alert("This is an image.");
-   });
+    var h1Tag = "header"
+    var pTag = "paragraph"
+    var imgTag = "image"
+
+    var tags = ["h1", "p", "img"]
+    var tagMeanings = ["header", "paragraph", "image"]
+
+    tags.forEach(function(tag) {
+      $(tag).click(function() {
+        index = tags.indexOf(tag);
+        alert("This is a " + tagMeanings[index]);
+      });
+    });
   });
+
+  //  $("h1").click(function() {
+  //    alert("This is a header.");
+  //  });
+  //  $("p").click(function() {
+  //    alert("This is a paragraph.");
+  //  });
+  //  $("img").click(function() {
+  //    alert("This is an image.");
+  //  });
+  // });
